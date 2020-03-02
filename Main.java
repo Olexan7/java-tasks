eefb9b052ce1cdab2a7ccb330693e2ccad63f616
olexan7 <ghoostt61@gmail.com>
2020-03-02 21:02:57 +0400

    feat: add solution task02

--- Main.java
+++ Main.java
@@ -1,35 +1,41 @@
 /**
- * Лекция 2. Задание 1: Генерация ошибок & Hello, World
+ * Лекция 2. Задание 2
+ * Генерация исключений корня от отрицательного числа
  * @autor Кондратьев Александр ВМ-41
  * @version 1.0
  */
 
 package com.company;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //привет мир
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите N: ");
+        int N = in.nextInt(); // количество случайных чисел
 
-        String unknownString = new String(); //пустая строка
-        unknownString = null;
+        byte [] arrayNumber = new byte [N]; // массив для хранения чисел длинной N
+        Random rand = new Random ();
+        rand.nextBytes(arrayNumber); //запоняем масссив значениями
 
-        try{
-            if(unknownString == null) {
-                throw new NullPointerException(); // обрабатываем исключение
+        for(int j = 0; j < N; j++){ // перебираем все числа
+            try{
+                int k = (int) arrayNumber[j]; // присваиваем значение переменной k
+                if(k < 0){
+                    throw new ArithmeticException("Элемент № " + j + ": " + " Error " + k + " <0"); //выбрасываем исключенее если k отрицательное
+                }
+
+                int q = (int) Math.sqrt(k);
+                if(q*q == k){
+                    System.out.println("Элемент № " + j + ": k = " + k); //вывести если q^2 равняется k
+                }
             }
-        }
-        catch (NullPointerException e){
-            System.out.println(e  + ": string is null"); //ввывод исключения
-        }
-
-       int[] falseArray = new int[5]; // массив длинной 5
-        try{
-            throw new ArrayIndexOutOfBoundsException(falseArray[5]); //обращаемся к несуществующему элементу
-        }
-        catch (ArrayIndexOutOfBoundsException e){
-            System.out.println(e +  ": element does not exist");  // сообщение об ошибке
-            throw e; // ошибка
+            catch (ArithmeticException e){
+                System.out.println(e); // выводим исключение, созданное в блоке try
+            }
         }
     }
 }
+
